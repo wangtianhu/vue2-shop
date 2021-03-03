724e8ed4d84ce3934e42870da569cd52b5613ab2
wangtianhu <wangtianhu@021.com>
2021-03-03 17:24:51 +0800

    webpack基本配置完成

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './index.scss';
+import pic from './assets/test.png';
 class Animal {
   constructor(name = 'aaa') {
     this.name = name;
@@ -6,5 +8,10 @@ class Animal {
     return this.name;
   }
 }
-
+console.log(66666);
+console.log(555);
 const dog = new Animal('dog');
+
+const img = document.createElement('img');
+img.src = pic;
+document.body.append(img);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const isDev = process.env.NODE_ENV === 'development';
-
+const path = require('path');
 const config = require('./public/config')[isDev ? 'dev' : 'build'];
+console.log('config', config);
 module.exports = {
   mode: 'development',
+  devtool: 'cheap-module-eval-source-map', //开发环境下用
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash:10].js',
+    publicPath: '/' // cdn的地址
+  },
   module: {
     rules: [
       {
@@ -24,6 +32,41 @@ module.exports = {
           }
         },
         exclude: /node_modules/
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: function () {
+                return [
+                  require('autoprefixer')({
+                    'overrideBrowserslist': ['>0.25%', 'not dead']
+                  })
+                ];
+              }
+            }
+          },
+          'sass-loader'
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|jpeg|webp|svg|eot|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10240, //10K
+              esModule: false,
+              name: '[name]_[hash:6].[ext]'
+            }
+          }
+        ],
+        exclude: /node_modules/
       }
     ]
   },
@@ -31,12 +74,20 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
-      filename: 'test.html',
+      filename: 'index.html',
       config: config.template,
       minify: {
         removeAttributeQuotes: false, //是否删除属性的双引号
         collapseWhitespace: false //是否折叠空白
       }
-    })
-  ]
+    }),
+    new CleanWebpackPlugin()
+  ],
+
+  devServer: {
+    port: '8085',
+    hot: true,
+    stats: 'errors-only', //终端只打印error
+    overlay: false
+  }
 };
